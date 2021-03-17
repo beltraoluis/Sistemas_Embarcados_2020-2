9a8391c1d26111f6e1799aaf79fc5427e5012bbd
Luís Henrique Beltrão Santana <beltraoluis@gmail.com>
2021-03-16 21:42:02 -0300

    tarefas exercicio 2 parte 3

--- rtos/Projects/tarefas/src/tarefas.c
+++ rtos/Projects/tarefas/src/tarefas.c
@@ -2,15 +2,17 @@
 #include "driverleds.h" // device drivers
 #include "cmsis_os2.h" // CMSIS-RTOS
 
-osThreadId_t blink_id1, blink_id2;
+osThreadId_t blink_id1, blink_id2, blink_id3, blink_id4;
 
 typedef struct blink_param{
   uint8_t led;
   uint32_t delay;
 } BlinkParam;
 
-BlinkParam d1 = {LED1, 100}; 
-BlinkParam d2 = {LED2, 100}; 
+BlinkParam d1 = {LED1, 200}; 
+BlinkParam d2 = {LED2, 300}; 
+BlinkParam d3 = {LED3, 500}; 
+BlinkParam d4 = {LED4, 700}; 
 
 void blinkTask(void *arg){
   uint8_t state = 0;
@@ -34,6 +36,8 @@ void main(void){
 
   blink_id1 = osThreadNew(blinkTask, &d1, NULL);
   blink_id2 = osThreadNew(blinkTask, &d2, NULL);
+  blink_id3 = osThreadNew(blinkTask, &d3, NULL);
+  blink_id4 = osThreadNew(blinkTask, &d4, NULL);
 
   if(osKernelGetState() == osKernelReady)
     osKernelStart();
